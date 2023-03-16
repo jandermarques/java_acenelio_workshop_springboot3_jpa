9aeae3cfc879903f2391498e4ce653ae67319594
Jander Ricardo Garcia Marques <jander.marques@gmail.com>
2023-03-16 19:05:42 -0300

    Exception handling - findById

---
+++ src/main/java/com/jander/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.jander.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not find. Id " + id);
+	}
+	
+}
